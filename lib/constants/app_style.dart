7a0cd83d4e85315eff42795ff87f13ce8695c1f3
Amyr <amyr.fezzeni@gmail.com>
2024-06-19 13:42:13 +0100

    first commit

---
+++ lib/constants/app_style.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+// App text Style
+TextStyle textstyle = GoogleFonts.nunito(color: Colors.black87, fontSize: 14);
+
+TextStyle titlestyle = GoogleFonts.poppins(
+    color: Colors.white, fontSize: 35, fontWeight: FontWeight.w800);
+
+
+
+// App colors
+const lightBgColor = Color.fromARGB(255, 255, 255, 255);
+const darkBgColor = Color.fromARGB(255, 23, 23, 23);
+
+const primaryColor = Color.fromARGB(212, 223, 0, 0);
+
+const primaryGradient = [
+  Color.fromARGB(255, 82, 113, 255),
+  Color.fromARGB(255, 91, 121, 251)
+];
+
+const palette = [
+  Color.fromRGBO(75, 135, 185, 1),
+  Color.fromRGBO(246, 114, 128, 1),
+  Color.fromRGBO(248, 177, 149, 1),
+  Color.fromRGBO(116, 180, 155, 1),
+  Color.fromRGBO(0, 168, 181, 1),
+  Color.fromRGBO(73, 76, 162, 1),
+  Color.fromRGBO(255, 205, 96, 1),
+  Color.fromRGBO(255, 240, 219, 1),
+  Color.fromRGBO(238, 238, 238, 1)
+];
+
+// Extra settings
+const double smallRadius = 8;
+const double bigRadius = 20;
+final defaultSmallRadius = BorderRadius.circular(smallRadius);
+final defaultBigRadius = BorderRadius.circular(bigRadius);
+
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/providers/data_provider.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => DataProvider())],
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+    );
+  }
+}
